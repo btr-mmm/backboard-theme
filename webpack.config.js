7038a5f09166e54478e5476c79d031543f8d6114
Ben Reiner <ben@mcguinnessmedia.com>
2025-02-25 16:55:52 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+module.exports = [
+  {
+    entry: {
+      main: ['./src/js/main.js', './src/css/main.scss'],
+    },
+    output: {
+      filename: './dist/bundle.min.js',
+      path: path.resolve(__dirname),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.(sass|scss)/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  includePaths: [nodeModulesPath],
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: './dist/main.min.scss',
+      }),
+    ],
+    optimization: {
+      minimizer: [`...`, new CssMinimizerPlugin()],
+    },
+  },
+];
+
