2a05c9ed450c77596a9f22fc487150d424e3edf7
Ben Reiner <ben@mcguinnessmedia.com>
2025-03-03 13:02:58 -0500

    Add nav menu

--- header.php
+++ header.php
@@ -9,6 +9,47 @@
 
 <body>
   <header>
-
+    <a href="<?php echo site_url(); ?>" class="logo">
+      Higher Learning Basketball
+    </a>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="<?php echo site_url('/training') ?>"
+            <?php if (is_page('training')) echo 'class="active"' ?>>
+            Training
+          </a>
+        </li>
+        <li>
+          <a
+            href="<?php echo site_url('/college-consulting') ?>"
+            <?php if (is_page('college-consulting')) echo 'class="active"' ?>>
+            College Consulting
+          </a>
+        </li>
+        <li>
+          <a
+            href="<?php echo site_url('/camps-and-clinics') ?>"
+            <?php if (is_page('camps-and-clinics')) echo 'class="active"' ?>>
+            Camps & Clinics
+          </a>
+        </li>
+        <li>
+          <a
+            href="<?php echo site_url('/leagues') ?>"
+            <?php if (is_page('leagues')) echo 'class="active"' ?>>
+            Leagues
+          </a>
+        </li>
+        <li>
+          <a
+            href="<?php echo site_url('/about') ?>"
+            <?php if (is_page('about')) echo 'class="active"' ?>>
+            About Us
+          </a>
+        </li>
+      </ul>
+    </nav>
   </header>
   <main>
