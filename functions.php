d34269a93ef8f046007e196c45a88106fe57d2c5
Ben Reiner <ben@mcguinnessmedia.com>
2025-03-03 11:25:29 -0500

    Add functions.php boilerplating

--- functions.php
+++ functions.php
@@ -1,56 +1,108 @@
 <?php
 
-/**
- * Perform all theme setup for the Backboard theme
- */
-class BackboardTheme
-{
+if (!class_exists('BackboardTheme')) {
   /**
-   * The current version of Backboard, set in style.css
-   * @var string The theme version
+   * Perform all theme setup for the Backboard theme
    */
-  private $themeVersion;
-
-  public function __construct()
+  class BackboardTheme
   {
-    $theme = wp_get_theme();
-    $this->themeVersion = $theme->get('Version');
+    /**
+     * The current version of Backboard, set in style.css
+     * @var string The theme version
+     */
+    private $themeVersion;
 
-    $this->hooks();
+    public function __construct()
+    {
+      $theme = wp_get_theme();
+      $this->themeVersion = $theme->get('Version');
+
+      $this->loadPostTypes();
+      $this->loadACFFields();
+
+      $this->hooks();
+    }
+
+    /**
+     * Setup actions and filters.
+     */
+    private function hooks()
+    {
+      // Enqueue styles & scripts
+      add_action('wp_enqueue_scripts', [$this, 'enqueue']);
+
+      // Add theme supports
+      add_action('after_setup_theme', [$this, 'themeSupports']);
+    }
+
+    /**
+     * Enqueue theme CSS stylesheets and JS scripts.
+     * For use in the wp_enqueue_scripts action hook.
+     */
+    public function enqueue()
+    {
+      wp_enqueue_style('backboard', get_template_directory_uri() . '/dist/main.min.css', [], $this->themeVersion);
+      wp_enqueue_script('backboard', get_template_directory_uri() . '/dist/bundle.min.js', ['jquery'], $this->themeVersion, true);
+    }
+
+    /**
+     * Add theme supports.
+     * For use in the after_setup_theme hook.
+     */
+    public function themeSupports()
+    {
+      add_theme_support('title-tag');
+      add_theme_support('automatic-feed-links');
+      add_theme_support('post-thumbnails');
+    }
+
+    /**
+     * Load all post types from ./inc/post-types
+     */
+    private function loadPostTypes()
+    {
+      $postTypesDirectory = get_template_directory() . '/inc/post-types';
+
+      if (!file_exists($postTypesDirectory)) {
+        return;
+      }
+
+      $postTypeFiles = glob($postTypesDirectory . '*.php');
+      if (!empty($postTypeFiles)) {
+        foreach ($postTypeFiles as $file) {
+          require_once $file;
+        }
+      }
+    }
+
+    /**
+     * Load all ACF field groups from ./inc/acf-fields
+     */
+    private function loadACFFields()
+    {
+      if (!function_exists('acf_add_local_field_group')) {
+        return;
+      }
+
+      $acfFieldsDirectory = get_template_directory() . '/inc/post-types';
+
+      if (!file_exists($acfFieldsDirectory)) {
+        return;
+      }
+
+      $acfFieldFiles = glob($acfFieldsDirectory . '*.php');
+      if (!empty($acfFieldFiles)) {
+        foreach ($acfFieldFiles as $file) {
+          require_once $file;
+        }
+      }
+    }
   }
 
-  /**
-   * Setup actions and filters.
-   */
-  private function hooks()
+  // Instantiate a new BackboardTheme instance
+  function initTheme()
   {
-    // Enqueue styles & scripts
-    add_action('wp_enqueue_scripts', [$this, 'enqueue']);
-
-    // Add theme supports
-    add_action('after_setup_theme', [$this, 'themeSupports']);
+    return new BackboardTheme();
   }
-
-  /**
-   * Enqueue theme CSS stylesheets and JS scripts.
-   * For use in the wp_enqueue_scripts action hook.
-   */
-  private function enqueue()
-  {
-    wp_enqueue_style('backboard', get_template_directory_uri() . '/dist/main.min.css', [], $this->themeVersion);
-    wp_enqueue_script('backboard', get_template_directory_uri() . '/dist/bundle.min.js', ['jquery'], $this->themeVersion, true);
-  }
-
-  /**
-   * Add theme supports.
-   * For use in the after_setup_theme hook.
-   */
-  private function themeSupports()
-  {
-    add_theme_support('title-tag');
-    add_theme_support('automatic-feed-links');
-    add_theme_support('post-thumbnails');
-  }
+  add_action('after_setup_theme', 'initTheme');
 }
-
-$backboard = new BackboardTheme();
