f4a5b18e58211a890cb4dad5dfecee18ea3511e4
Ben Reiner <ben@mcguinnessmedia.com>
2025-03-04 16:15:19 -0500

    Add CTA styles

--- template-parts/cta.php
+++ template-parts/cta.php
@@ -1,13 +1,14 @@
 <?php while (have_rows('cta', 'option')):
   the_row();
-  $link = get_sub_field('link'); ?>
-  <section class="call-to-action">
+  $link = get_sub_field('link');
+  $bg = get_sub_field('background'); ?>
+  <section class="cta" style="background-image: url(<?php echo $bg['url'] ?>);">
     <div class="wrapper">
       <h2>
         <?php the_sub_field('title') ?>
       </h2>
       <?php the_sub_field('content') ?>
-      <a href="<?php echo esc_url($link['url']) ?>" class="btn btn-primary" target="<?php echo $link['target'] ?>">
+      <a href="<?php echo esc_url($link['url']) ?>" class="button" target="<?php echo $link['target'] ?>">
         <?php echo esc_attr($link['title']) ?>
       </a>
     </div>
