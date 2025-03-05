57be5d4b908cae8508fb11651f757f68df67dab7
Ben Reiner <ben@mcguinnessmedia.com>
2025-03-05 11:37:40 -0500

    Add homepage markup

--- front-page.php
+++ front-page.php
@@ -1,14 +1,29 @@
 <?php get_header(); ?>
 
-<h1>Hello from front-page</h1>
+<h1 class="sr-only">Hello from front-page</h1>
 
 <?php $sectionCount = 1;
-while (have_rows('homepage_content')):
-  the_row(); ?>
-  <section id="section<?php echo $sectionCount ?>" class="homepage-section">
+while (have_rows('homepage_sections')):
+  the_row();
+  $button = get_sub_field('button');
+  $img = get_sub_field('image');
+  $bg = get_sub_field('background') ?>
+  <section
+    id="section<?php echo $sectionCount ?>"
+    class="homepage-section"
+    data-nav-title="<?php echo esc_attr($button['title']) ?>"
+    style="background-image: url(<?php echo esc_url($bg['url']) ?>);">
     <h2><?php the_sub_field('title') ?></h2>
-    <div class="content">
-      <!-- TODO: Add content markup -->
+    <div class="hover-content">
+      <img
+        src='<?php echo esc_url($img['url']) ?>'
+        alt='<?php if (isset($img['alt'])) echo esc_attr($img['alt']) ?>'>
+      <div class="content">
+        <?php the_sub_field('body'); ?>
+        <a href="<?php echo esc_url($button['url']) ?>" class="button" target="<?php echo $link['target'] ?>">
+          <?php echo esc_attr($button['title']) ?>
+        </a>
+      </div>
     </div>
   </section>
 <?php $sectionCount++;
